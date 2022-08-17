3ea25e34a950899e4a6a571a6c259c2971e3479a
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-08-17 05:49:08 -0300

    Brincadeira Alice

---
+++ acao.js
@@ -0,0 +1,21 @@
+function clicouNao(){
+    document.getElementById('resposta_pergunta').innerHTML = 'Se você não quer, não vai ganhar!'
+}
+
+function moveu1(){
+    var btn = document.getElementById('comClique')
+    btn.style.top='60px';
+}
+
+function moveu2(){
+    var btn = document.getElementById('comClique')
+    btn.style.top='-1   0px'
+}
+
+var btn = document.getElementById('comClique')
+btn.addEventListener("mouseover",comClique)
+btn.addEventListener('mouseover',moveu1)
+btn.addEventListener('mouseout',moveu2)
+
+
+
